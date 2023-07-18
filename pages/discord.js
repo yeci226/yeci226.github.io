3a330a67d118e05e724f069f9474179bf77b6732
yeci226 <shawnyin226@gmail.com>
2023-07-18 22:20:50 +0800

    Change to static

--- pages/discord.js
+++ pages/discord.js
@@ -1,12 +1,19 @@
-export async function getServerSideProps() {
-  return {
-    redirect: {
-      destination: "https://discord.gg/qaWtVXq82d",
-      permanent: false,
-    },
-  };
-}
+import { useEffect } from "react";
+import Router from "next/router";
 
-export default function DiscordRedirect() {
+function DiscordRedirect() {
+  useEffect(() => {
+    Router.replace("https://discord.gg/qaWtVXq82d");
+  }, []);
+
   return null;
 }
+
+DiscordRedirect.getInitialProps = async () => ({
+  redirect: {
+    destination: "https://discord.gg/qaWtVXq82d",
+    permanent: false,
+  },
+});
+
+export default DiscordRedirect;
