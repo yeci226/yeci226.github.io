fd0aea625a144f70a615a569867131a50e4d77e5
yeci226 <shawnyin226@gmail.com>
2023-07-19 22:38:48 +0800

    Menu

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,7 @@ import { DiscordClick } from "../js/cookiesocute";
 export default function Header() {
   const router = useRouter();
   const [isOnHomepage, setIsOnHomepage] = useState(true);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   useEffect(() => {
     setIsOnHomepage(router.pathname === "/");
@@ -23,15 +24,23 @@ export default function Header() {
       </div>
       <nav className="menu">
         <div className="link-wrapper">
-          <a className="link" href="/about">
-            關於AET
-          </a>
-          <a className="link" href="/about">
-            賽制介紹
-          </a>
-          <a className="link" href="/about">
-            比賽直播
-          </a>
+          <div
+            className="link dropdown-link"
+            onClick={() => setDropdownVisible(!dropdownVisible)}
+          >
+            <a>賽事資訊</a>
+            <ul className={`dropdown ${dropdownVisible ? "visible" : ""}`}>
+              <li>
+                <a href="/about">關於AET</a>
+              </li>
+              <li>
+                <a href="/rules">賽制介紹</a>
+              </li>
+              <li>
+                <a href="/live">比賽直播</a>
+              </li>
+            </ul>
+          </div>
           <button className={styles.discord_button} onClick={DiscordClick}>
             Discord
           </button>
@@ -58,12 +67,14 @@ export default function Header() {
           transition: transform 0.5s;
         }
         .logo {
+          user-select: none;
           cursor: pointer;
         }
         .logo-container.hidden {
           transform: translateX(-150%);
         }
         .text {
+          user-select: none;
           margin-left: 10px;
           font-size: 24px;
           color: #fff;
@@ -71,7 +82,13 @@ export default function Header() {
         .menu {
           display: flex;
         }
+        .link-wrapper {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+        }
         .link-wrapper a {
+          user-select: none;
           text-decoration: none;
           margin-right: 30px;
           border-radius: 10px;
@@ -93,6 +110,27 @@ export default function Header() {
         }
         .link-wrapper a:hover::after {
           right: 0;
+        }
+        .dropdown-link {
+          position: relative;
+        }
+        .dropdown {
+          max-height: 0;
+          overflow: hidden;
+          position: absolute;
+          list-style-type: none;
+          padding: 0 0;
+          transition: max-height 0.5s ease, padding 0.5s ease;
+        }
+        .dropdown.visible {
+          max-height: 200px;
+          padding: 15px 0;
+        }
+        .dropdown li a {
+          display: block;
+          padding: 5px 10px;
+          color: #fff;
+          text-decoration: none;
         }
       `}</style>
     </div>
---
+++ pages/live.js
@@ -0,0 +1,82 @@
+import Head from "next/head";
+import styles from "../styles/about.module.css";
+import Header from "../components/Header";
+import { useEffect, useState } from 'react';
+import { isMobileDevice } from '../js/cookiesocute';
+
+
+export default function About() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(isMobileDevice());
+  }, []);
+
+  const [staffMembers, setStaffMembers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/crew.json');
+        const data = await response.json();
+        setStaffMembers(data);
+      } catch (error) {
+        console.error('Error fetching staff data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>關於 AET</title>
+        <link rel="icon" href="/icon.ico" />
+        <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+      </Head>
+
+
+      {isMobile ? (
+        <div className={styles.background_image} style={{ backgroundImage: 'url(/bg_img.png)' }}/>
+      ) : (
+        <div className={styles.background_video}>
+          <video autoPlay loop muted style={{ width: "100%", height: "100%", objectFit: "cover" }}>
+            <source src="/bg.webm" type="video/webm"/>
+          </video>
+        </div>
+      )}
+
+
+      <main>
+
+        <Header />
+          
+        <img src="/logoNText.png" className={styles.logo} draggable="false"/>
+        <img src="/logo2022.png" className={styles.logo} draggable="false"/>
+        <img src="/logo2021.png" className={styles.logo} draggable="false"/>
+          
+        <h1 className={styles.title}>AET 荒野亂鬥亞洲交流錦標賽</h1>
+          
+        <div className={styles.about_textSpace}>
+          AET集結來自東亞各處熱愛《荒野亂鬥》的玩家，舉辦一年一次的交流錦標賽。迄今已連續舉辦三屆，每屆都受到廣大玩家的熱情支持。馬上加入，和我們一同享受亂鬥的樂趣！
+        </div>
+        <div className={styles.staffContainer}>
+          <h1>工作人員</h1>
+            <div className={styles.staffList}>
+              {staffMembers.map((staff) => (
+                <div key={staff.id} className={styles.staffMember}>
+                  <img src={staff.avatar} alt={staff.name} className={styles.avatar} draggable="false" />
+                  <h3>{staff.name}</h3>
+                  <p>{staff.job}</p>
+                </div>
+                ))}
+            </div>
+        </div>
+    
+      </main>
+      
+    </div>
+  );
+}
