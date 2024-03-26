dcb375951a9af83f5b4b615dca53d56d55923419
yeci226 <shawnyin226@gmail.com>
2024-03-26 16:05:04 +0800

    marmot again

--- pages/books.js
+++ pages/books.js
@@ -6,6 +6,11 @@ import { useEffect, useState } from "react";
 
 export default function Books() {
   const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterType, setFilterType] = useState("all");
+  const [loggedInUsername, setLoggedInUsername] = useState(null);
+  const [showAvailableOnly, setShowAvailableOnly] = useState(false);
+  const borrowBookLimit = 3;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,7 +21,42 @@ export default function Books() {
     };
 
     fetchData();
+
+    const cookies = document.cookie.split("; ");
+    const loggedInUserCookie = cookies.find((cookie) =>
+      cookie.startsWith("loggedInUser=")
+    );
+    const loggedInUser = loggedInUserCookie
+      ? loggedInUserCookie.split("=")[1]
+      : null;
+    setLoggedInUsername(loggedInUser);
   }, []);
+
+  const borrowedBooksCount = books.filter(
+    (book) => book.borrower === loggedInUsername
+  ).length;
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCheckboxChange = () => {
+    setShowAvailableOnly(!showAvailableOnly);
+  };
+
+  const filteredBooks = books.filter((book) => {
+    const titleMatch = book.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    if (filterType === "all") {
+      return titleMatch && (!showAvailableOnly || book.status === null);
+    } else if (filterType === "available") {
+      return titleMatch && book.status === null;
+    } else if (filterType === "borrowed") {
+      return titleMatch && book.status !== null;
+    }
+  });
 
   return (
     <div className={styles.container}>
@@ -36,11 +76,42 @@ export default function Books() {
       <main>
         <Header />
         <h1 className={styles.title}>書籍詳情</h1>
-
         <div className={styles.textSpace}>這是顯示書籍詳細資訊的地方</div>
+
+        <input
+          className={styles.searchBar}
+          type="text"
+          placeholder="搜尋書籍..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+
+        <div className={styles.checkbox}>
+          <div className={styles.check}>
+            <input
+              id="showAvailableOnly"
+              type="checkbox"
+              checked={showAvailableOnly}
+              onChange={handleCheckboxChange}
+            />
+            <label htmlFor="showAvailableOnly"></label>
+          </div>
+          <label className={styles.checkboxLabel} htmlFor="showAvailableOnly">
+            {showAvailableOnly ? "顯示可借用書籍" : "顯示全部書籍"}
+          </label>
+
+          <span className={styles.borrowBookLimitLabel}>
+            已借用書籍 {`${borrowedBooksCount}/${borrowBookLimit}`}
+          </span>
+        </div>
+
+        {filteredBooks.length === 0 && (
+          <div className={styles.noResults}>沒有相關書籍搜尋結果</div>
+        )}
+
         <div className={styles.bookContainer}>
           <div className={styles.bookList}>
-            {books.map((book) => (
+            {filteredBooks.map((book) => (
               <a
                 key={book.id}
                 href={`/book/${book.id}`}
